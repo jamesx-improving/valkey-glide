1f735d46e8eb30f54251b6bc8948f87298c9c9d3
James Xin <james.xin@improving.com>
2025-01-31 12:18:09 -0800

    add darwin flag, move to GH hosted arm64 for matrix
    Signed-off-by: James Xin <james.xin@improving.com>

--- go/api/response_handlers.go
+++ go/api/response_handlers.go
@@ -4,6 +4,7 @@ package api
 
 // #cgo LDFLAGS: -lglide_rs
 // #cgo !windows LDFLAGS: -lm
+// #cgo darwin LDFLAGS: -framework Security
 // #cgo linux, amd64 LDFLAGS: -L${SRCDIR}/../rustbin/x86_64-unknown-linux-gnu
 // #cgo linux, arm64 LDFLAGS: -L${SRCDIR}/../rustbin/aarch64-unknown-linux-gnu
 // #cgo darwin, arm64 LDFLAGS: -L${SRCDIR}/../rustbin/aarch64-apple-darwin
