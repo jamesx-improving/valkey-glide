05436e16ef2b464010d544d8f78dde0874b1fe7e
James Xin <james.xin@improving.com>
2025-01-31 10:52:10 -0800

    add double paths for all LDFLAGS
    Signed-off-by: James Xin <james.xin@improving.com>

--- go/api/glide_client.go
+++ go/api/glide_client.go
@@ -7,6 +7,7 @@ package api
 // #cgo linux, amd64 LDFLAGS: -L${SRCDIR}/../rustbin/x86_64-unknown-linux-gnu
 // #cgo linux, arm64 LDFLAGS: -L${SRCDIR}/../rustbin/aarch64-unknown-linux-gnu
 // #cgo darwin, arm64 LDFLAGS: -L${SRCDIR}/../rustbin/aarch64-apple-darwin
+// #cgo LDFLAGS: -L../target/release
 // #include "../lib.h"
 import "C"
 
